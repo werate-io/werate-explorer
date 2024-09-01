f87cf265d254294e802f7b8685b37ae7262e272b
Kenzo <kenzoheye@gmail.be>
2024-09-01 17:54:53 +0200

    Init

---
+++ src/components/map.tsx
@@ -0,0 +1,39 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LatLngExpression, LatLngTuple } from "leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import "leaflet-defaulticon-compatibility";
+
+interface MapProps {
+  posix?: LatLngExpression | LatLngTuple;
+  zoom?: number;
+}
+
+const defaults = {
+  zoom: 7,
+  posix: { lat: 50.85, lng: 4.348 },
+};
+
+const Map = (Map: MapProps) => {
+  const { zoom = defaults.zoom, posix = defaults.posix } = Map;
+
+  return (
+    <MapContainer
+      center={posix}
+      zoom={zoom}
+      scrollWheelZoom={false}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={posix} draggable={false}>
+        <Popup>Test</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
