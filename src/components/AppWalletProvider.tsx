fa4a539fe14bfbe7ff22ac2438fbcbebec624e38
RateBuilder <daniel@werate.io>
2024-09-06 03:43:05 -0400

    update: add AppWalletProvider component

---
+++ src/components/AppWalletProvider.tsx
@@ -0,0 +1,44 @@
+"use client";
+ 
+import React, { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { PhantomWalletAdapter, LedgerWalletAdapter, TorusWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+ 
+// Default styles that can be overridden by your app
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+// imports here
+ 
+export default function AppWalletProvider({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    const wallets = useMemo(
+      () => [
+        new PhantomWalletAdapter(),
+        new LedgerWalletAdapter(),
+        new TorusWalletAdapter(),
+        new SolflareWalletAdapter()
+      ],
+      [network],
+    );
+
+    return (
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            {children}
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    );
+  }
