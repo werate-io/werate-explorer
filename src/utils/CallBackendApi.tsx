76548f434227825ec5ffdd82e94e77551fac097c
RateBuilder <daniel@werate.io>
2024-09-08 23:46:32 -0400

    update: created CallBackendApi.tsx file in utils folder and defined postData and getData functions to modulize backend api calling

---
+++ src/utils/CallBackendApi.tsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const postData = async (backend_api: string, data?: object) => {
+  try {
+    const response = await axios.post(
+      process.env.API_PREFIX + backend_api,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if(response.data.success)
+      {
+        return response.data.success
+      }
+    else{
+      return response.data.message
+    }
+  } catch (error) {
+    console.error('Error posting data:', error);
+  }
+};
+
+export const getData = async (backend_api: string, params?: object) => {
+  try {
+    const response = await axios.get(
+      backend_api,
+      {
+        params,  // Pass params here instead of data
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if (response.data.success) {
+      return response.data.success;
+    } else {
+      return response.data.message;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
