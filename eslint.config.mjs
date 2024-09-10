657562a8ba794db71c44749a70d06ac9a3f818f0
RateBuilder <daniel@werate.io>
2024-09-10 19:00:12 -0400

    update: added some settings to eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,72 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime'
+  ),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {
+      pluginReact
+    },
+    languageOptions: {
+      ecmaVersion: 5,
+      sourceType: 'script',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals: { ...globals.browser, ...globals.node }
+    },
+
+    settings: {
+      react: {
+        version: 'detect' // Automatically detect the React version
+      }
+    },
+
+    rules: {
+      'max-len': [
+        'error',
+        {
+          code: 200,
+          ignoreUrls: true,
+          ignoreStrings: true
+        }
+      ],
+
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'objectLiteralProperty',
+          format: ['camelCase', 'snake_case']
+        }
+      ]
+    },
+
+    ignores: ['**/*.config.mjs'] //remove *.config.mjs files from this configuration object
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat.recommended
 ];
