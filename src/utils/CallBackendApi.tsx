80c395b13ab67e010b7c079cd8d42fe2667ce116
RateBuilder <daniel@werate.io>
2024-09-13 13:29:59 -0400

    update: function update in CallBanckendApi

--- src/utils/CallBackendApi.tsx
+++ src/utils/CallBackendApi.tsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({path: '.env'});
 
 export const postData = async (backend_api: string, data?: object) => {
   try {
     const response = await axios.post(
-      process.env.API_PREFIX + backend_api,
+      `http://localhost:8080${backend_api}`,
       data,
       {
         headers: {
@@ -25,6 +25,20 @@ export const postData = async (backend_api: string, data?: object) => {
   } catch (error) {
     console.error('Error posting data:', error);
   }
+  axios.post(`http://localhost:8080${backend_api}`, data)  // Backend API on Fastify
+    .then((response) => {
+      if(response.data.success)
+        {
+          return response.data.success
+        }
+      else{
+        return response.data.message
+      }
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+
 };
 
 export const getData = async (backend_api: string, params?: object) => {
