702b3c8c669fb406c188adcd5e73722b0e430534
Talhaaa99 <talha.toufeeq99@gmail.com>
2024-10-04 15:42:19 +0530

    proxy fix

--- src/services/werate-api.ts
+++ src/services/werate-api.ts
@@ -5,8 +5,7 @@ import { getCookie } from 'cookies-next';
 dotenv.config({ path: '.env' });
 
 const instance = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL
-  baseURL: 'https://api.werate.io/https://cors-anywhere.herokuapp.com/'
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL
 });
 
 const getBearerToken = (): string => {
