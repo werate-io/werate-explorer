b9e8fbc0df3ee0b5a82dee625dca6028ee0a221f
Siebe Verschaeve <siebe@werate.io>
2024-10-07 11:06:52 +0200

    fix: Fixed cors issues. (#45)
    Co-authored-by: Siebe Verschaeve <verschaeves@delawareconsulting.com>

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://explorer.werate.io/:path*' // Proxy to Backend
+      }
+    ];
+  }
 };
